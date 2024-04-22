5619876dd19253192c91cdde8486aff454f7d6d3
Saviour Assandoh <asandohs@gmail.com>
2024-04-21 19:27:03 -0700

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#This is the hello module
+
+sudo apt install python3
+
+python3 -m venv venv
+
+venv\bin\activate
+
+cd venv
+
+python3 -m pip install --upgrade pip
+
+pip install flask
+
+echo "from flask import Flask\napp = Flask(__name__)\n\n@app.route("/",strict_slashes=False)\ndef hello():\n\treturn "Hello HBNB!"\n\nif (__name__) == "__main__":\n\tapp.run(host="0.0.0.0", port=5000)" > app.py
+
+export FLASK_APP=app
+
+flask run
