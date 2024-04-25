5222d1bbe64495d587e6d6148adc82e8d1873e93
Saviour Assandoh <asandohs@gmail.com>
2024-04-24 17:25:52 -0700

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+"""This is the hello module"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+app.route("/", strict_slashes=False)
+def hello():
+    """Display Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HB"""
+    return "HBNB"
+
+
+if (__name__) == "__main__":
+    app.run(host="0.0.0.0", port=5000)
