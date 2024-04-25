96dbdc23cc9325c41e8c8d792083e76de71ed209
Saviour Assandoh <asandohs@gmail.com>
2024-04-24 17:47:44 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+"""This is the hello module"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+app.route('/', strict_slashes=False)
+def hello():
+    """Display Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HB"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Display of c is fun"""
+    text = text.replace("_", " ")
+    return "C " + text
+
+if (__name__) == "__main__":
+    app.run(host='0.0.0.0', port=5000)
