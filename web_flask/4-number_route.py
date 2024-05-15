83a99469e53ca2db04e5884294bfbb81f5029d33
Saviour Assandoh <asandohs@gmail.com>
2024-05-15 17:25:46 +0000

    mandatory

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,5 +28,11 @@ def python(text):
     text = text.replace("_", " ")
     return "Python " + text
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+        return f"{n} is a number"
+
+
+
 if (__name__) == "__main__":
     app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""This is the hello module"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HB"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Display of c is fun"""
+    text = text.replace("_", " ")
+    return "C " + text
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """Display of c is fun"""
+    text = text.replace("_", " ")
+    return "Python " + text
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+        return f"{n} is a number"
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('number.html', n=n)
+
+
+if (__name__) == "__main__":
+    app.run(host='0.0.0.0', port=5000)
